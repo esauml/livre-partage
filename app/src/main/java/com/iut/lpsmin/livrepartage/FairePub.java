e3544860c7c3aba7d1b9078adfd494ddab9cd4ed
Cherif-Betchim <cherif-betchim@hotmail.com>
2020-12-04 10:03:51 +0100

    angel merge

---
+++ app/src/main/java/com/iut/lpsmin/livrepartage/FairePub.java
@@ -0,0 +1,18 @@
+package com.iut.lpsmin.livrepartage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class FairePub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faire_pub);
+    }
+
+    public void Enregistrer(View view) {
+    }
+}
