8c91298174cbc59e3eebddcd7274e19de9fd41fd
Angel Contreras <c3po.angel@gmail.com>
2020-12-04 18:01:02 +0100

    effacer

--- app/src/main/java/com/iut/lpsmin/livrepartage/MainActivity.java
+++ app/src/main/java/com/iut/lpsmin/livrepartage/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent =new Intent(this, FairePub.class);
-        startActivity(intent);
     }
 
     public void logout(View view) {
