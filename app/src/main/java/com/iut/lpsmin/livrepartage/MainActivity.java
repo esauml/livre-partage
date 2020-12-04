f058ee7cd14418c91a0ca0331a0320dee1818081
Cherif-Betchim <cherif-betchim@hotmail.com>
2020-12-04 12:07:24 +0100

    WIP on connection to Data base

--- app/src/main/java/com/iut/lpsmin/livrepartage/MainActivity.java
+++ app/src/main/java/com/iut/lpsmin/livrepartage/MainActivity.java
@@ -2,7 +2,11 @@ package com.iut.lpsmin.livrepartage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut(); //so i can logout
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
