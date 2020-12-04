0c5aacfae9db850329084388b0d080b0d5357525
Angel Contreras <c3po.angel@gmail.com>
2020-12-04 10:09:12 +0100

    rien

--- app/src/main/java/com/iut/lpsmin/livrepartage/FairePub.java
+++ app/src/main/java/com/iut/lpsmin/livrepartage/FairePub.java
@@ -3,6 +3,7 @@ package com.iut.lpsmin.livrepartage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FairePub extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class FairePub extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faire_pub);
+    }
+
+    public void Enregistrer(View view) {
     }
 }
